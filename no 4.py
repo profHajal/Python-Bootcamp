03e56d0180cfe613f2dc6745f1c204cde85c5f4c
whoafridi <35966401+whoafridi@users.noreply.github.com>
2018-02-08 22:17:05 +0600

    Add files via upload

---
+++ no 4.py
@@ -0,0 +1,13 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+number = int(input("Please Enter a Number : "))
+x = int(number/2)*2;
+if(x == number):
+    print("Number is Even")
+else:
+    print("Number is Odd")
+
