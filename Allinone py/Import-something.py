faa9debe78b5e6c0f0500abeda0ffbcbec1aa859
Md. Rafiuzzaman Bhuiyan Afridi <35966401+whoafridi@users.noreply.github.com>
2018-03-07 23:18:14 +0600

    Add files via upload

---
+++ Allinone py/Import-something.py
@@ -0,0 +1,17 @@
+## Import 
+
+## Find Factorial 
+
+import math
+num = int(input())
+n = math.factorial(num)
+
+print(n)
+
+## Find Sqrt
+
+import math
+num = int(input())
+n1 = math.sqrt(num)
+
+print(n1)
