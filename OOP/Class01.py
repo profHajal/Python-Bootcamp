36791a0fcbd3d07341d750fe7af5b2f9a4a03195
Md. Rafiuzzaman Bhuiyan Afridi <35966401+whoafridi@users.noreply.github.com>
2019-01-17 23:41:56 +0600

    #01

---
+++ OOP/Class01.py
@@ -0,0 +1,16 @@
+class Car:
+    name = 'Toyota'
+    color = 'white'
+    
+    def start():
+        print('Starting the engine !')
+        
+print('Name of the car : ',Car.name)
+print('Color : ',Car.color)
+
+Car.start()
+
+# output
+Name of the car :  Toyota
+Color :  white
+Starting the engine !
