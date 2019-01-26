b932133aa8a00a9239514269ea722faebfce67ca
Md. Rafiuzzaman Bhuiyan Afridi <35966401+whoafridi@users.noreply.github.com>
2019-01-27 00:01:38 +0600

    # 02

---
+++ OOP/Class02.py
@@ -0,0 +1,23 @@
+class Car:
+    def __init__(self,name,color):  ## __init__() in py as contructor
+      self.name = name
+      self.color = color
+      
+    def start(self):
+      return 'Starting with it !'
+      
+my_car = Car('BMW' , 'Black')
+print(my_car)
+print(my_car.name)
+print(my_car.color)
+print(my_car.start())
+
+
+## Output >>
+<__main__.Car object at 0x7f1ae81612e8>
+BMW
+Black
+Starting with it
+
+
+
